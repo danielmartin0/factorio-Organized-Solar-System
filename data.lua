8a5b377708177b356755ea19b91c8b1328494232
danielmartin0 <github@danielgmartin.com>
2025-02-07 14:31:11 +0000

    see changelog.txt

--- data.lua
+++ data.lua
@@ -1,3 +1,5 @@
+local asteroid_util = require("__space-age__.prototypes.planet.asteroid-spawn-definitions")
+
 local vanilla = require("vanilla")
 
 for name, tier in pairs(vanilla.vanilla_planet_tiers) do
@@ -9,5 +11,29 @@ end
 for name, tier in pairs(vanilla.vanilla_location_tiers) do
 	if data.raw["space-location"][name] then
 		data.raw["space-location"][name].tier = tier
+	end
+end
+
+if settings.startup["Tiered-Solar-System-ensure-vanilla-planets-connected"].value then
+	log("Tiered-Solar-System: Ensuring vanilla planets are connected")
+	if data.raw["space-connection"]["gleba-fulgora"] then
+		log("Tiered-Solar-System: Ensuring Gleba and Fulgora are connected")
+		local connection = data.raw["space-connection"]["gleba-fulgora"]
+		connection.redrawn_connections_keep = true
+		connection.redrawn_connections_rescale = true
+	end
+
+	if data.raw["space-connection"]["nauvis-vulcanus"] then
+		log("Tiered-Solar-System: Ensuring Nauvis and Vulcanus are connected")
+		local connection = data.raw["space-connection"]["nauvis-vulcanus"]
+		connection.redrawn_connections_keep = true
+		connection.redrawn_connections_rescale = true
+	end
+
+	if data.raw["space-connection"]["vulcanus-gleba"] then
+		log("Tiered-Solar-System: Ensuring Vulcanus and Gleba are connected")
+		local connection = data.raw["space-connection"]["vulcanus-gleba"]
+		connection.redrawn_connections_keep = true
+		connection.redrawn_connections_rescale = true
 	end
 end
---
+++ settings-updates.lua
@@ -0,0 +1,8 @@
+data:extend({
+	{
+		type = "bool-setting",
+		name = "Tiered-Solar-System-ensure-vanilla-planets-connected",
+		setting_type = "startup",
+		default_value = false,
+	},
+})
